1e5fa7cea148a7ac75931b935baa61b5fd755aa5
Guilherme <guilherme_ip1@hotmail.com>
2025-06-11 23:14:35 -0300

    Desafio concluido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner Scanner =new Scanner(System.in);
+
+        //Exibir as mensagens para o usuario        System.out.println("Bem vindo ao Banco Teste!");
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = Scanner.nextInt(); // Obter o número da conta
+
+        System.out.print("Digite o número da agência: ");
+        String numeroAgencia = Scanner.next();
+
+        System.out.print("Digite o nome: ");
+        Scanner.nextLine();
+        String nomeCliente = Scanner.nextLine();
+
+        System.out.print("Digite o saldo: ");
+        String saldoEntrada = Scanner.nextLine();
+        double saldo = ConvertDouble(saldoEntrada);
+
+        System.out.println("Obrigado por informar os dados da conta!");
+        System.out.println("Olá, " + nomeCliente + ",obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
+
+    private static double ConvertDouble(String saldo) {
+        String saldoEntrada = saldo.replace(",", ".");
+        try {
+            return Double.parseDouble(saldoEntrada);
+        } catch (NumberFormatException e) {
+            System.out.println("Erro ao converter o saldo.");
+            return 0.0;
+        }
     }
 }
